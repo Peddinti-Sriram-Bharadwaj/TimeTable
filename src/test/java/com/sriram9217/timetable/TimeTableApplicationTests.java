f4268c10b3125cc381815318e549ac7fccc92a45
Sriram Bharadwaj <sriram9217@gmail.com>
2024-11-23 14:49:02 +0530

    initial commit

---
+++ src/test/java/com/sriram9217/timetable/TimeTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sriram9217.timetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
