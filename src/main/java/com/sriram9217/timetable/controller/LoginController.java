a842d10d8fde3dce281d60450266f8350e8025eb
Sriram Bharadwaj <sriram9217@gmail.com>
2024-11-24 12:48:48 +0530

    updated cors to accept requests from local development server for client

--- src/main/java/com/sriram9217/timetable/controller/LoginController.java
+++ src/main/java/com/sriram9217/timetable/controller/LoginController.java
@@ -5,13 +5,11 @@ import com.sriram9217.timetable.service.StudentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@CrossOrigin(origins = "http://localhost:3000")
 public class LoginController {
     private final StudentService studentService;
 
@@ -21,6 +19,7 @@ public class LoginController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login (@RequestBody @Valid LoginRequest request){
+        System.out.println("recieved login request: " + request);
         return ResponseEntity.ok(studentService.login(request));
     }
 
